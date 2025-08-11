c2fe3e080fd694dec9cd6206b939f5d4e3b4a49c
WaithakaGuru <waithakaoffices@gmail.com>
2025-08-11 23:23:18 +0300

    Optimize the siftDown method by adding a variation of the 'optima' - a function to choose the most appropiate child to interchange with the parent

--- nonLinear_data_structures/heaps/BaseClasses.ts
+++ nonLinear_data_structures/heaps/BaseClasses.ts
@@ -3,8 +3,11 @@ import { HeapMethodsDeclarations } from "./heapDeclarations";
 type HeapType = "MAX" | "MIN";
 
 const Comparers = {
-    MAX : (i: number, j: number) => i>j,
-    MIN : (i: number, j: number) => i<j
+    MAX:{ option:(i: number, j: number) => i>j, 
+        optima: (k: number, l: number, heap: number[])=> (heap[k] >= heap[l])? k : l},
+    MIN: {option: (i: number, j: number) => i<j, 
+        optima: (k: number, l: number, heap: number[]) => (heap[k] <= heap[l]) ? k : l
+    }
 }
 
 export abstract class Heap extends HeapMethodsDeclarations {
@@ -37,10 +40,10 @@ export abstract class Heap extends HeapMethodsDeclarations {
     }
     
     update(idx: number, val: number) {
-        const comp = Comparers[this.type];
+        const comp = Comparers[this.type].option;
         if(val === this.heap[idx]) return
         else if(this.len() === 0) this.heap.push(val);
-        else if(idx === 0 && comp(val, this.heap[0]) /*val < this.heap[0]*/) this.heap[0] = val;
+        else if(idx === 0 && comp(val, this.heap[0])) this.heap[0] = val;
         else if(idx === 0) {
             this.heap[idx] = val;
             this.siftDown(idx)
@@ -57,7 +60,7 @@ export abstract class Heap extends HeapMethodsDeclarations {
     }
 
     protected siftUp(i: number): void {     
-        let parent = Math.floor((i-1)/2), comp = Comparers[this.type];
+        let parent = Math.floor((i-1)/2), comp = Comparers[this.type].option;
         while(i > 0 && comp(this.heap[i], this.heap[parent])){
             [this.heap[i], this.heap[parent]] = [this.heap[parent], this.heap[i]]
             i = parent;
@@ -66,14 +69,13 @@ export abstract class Heap extends HeapMethodsDeclarations {
     }
     
     protected siftDown(i: number): void {
-        const opposite = this.type === "MAX"? "MIN" : "MAX", comp = Comparers[opposite];
-        const min = (k: number, j: number) => this.heap[k] <= this.heap[j] ? k : j;
-    
+        const opposite = this.type === "MAX"? "MIN" : "MAX", comp = Comparers[opposite].option;
+        const optimalChild = Comparers[this.type].optima
         let left = 2 * i + 1, right = 2 * i + 2;
         
         while(true){
             if(left > this.len()) return;
-            let less = (right > this.len()) ? left : min(left, right)
+            let less = (right > this.len()) ? left : optimalChild(left, right, this.heap)
             if(comp(this.heap[i], this.heap[less])){
                 [this.heap[i], this.heap[less]] = [this.heap[less], this.heap[i]];
                 i = less;
--- nonLinear_data_structures/heaps/tests.ts
+++ nonLinear_data_structures/heaps/tests.ts
@@ -4,9 +4,9 @@ import minHeap from "./minHeap";
 const trial2 = [12, 7, 25, 3, 18, 9, 2, 15, 6, 20];
 
 const heap1 = new minHeap ();
-heap1.heapify(trial2);
-console.log(heap1.heap);
-// heap1.pluck();
+heap1.heapify([4,3,2,1]);
+// console.log(heap1.heap);
+heap1.pluck();
 // heap1.pluck();
 // heap1.pluck();
 // console.log(heap1.peek());
@@ -17,9 +17,9 @@ console.log(heap1.heap);
 // console.log(heap1.heap);
 
 const heap2 = new MaxHeap();
-heap2.heapify([1,2,3,4,5,8]);
+heap2.heapify([1,2,3,4]);
 console.log(heap2.heap);
-console.log(heap2.peek());
+// console.log(heap2.peek());
 // heap2.pluck()
 // console.log(heap2.heap)
 
